b8f27feb8f7431a4c367d288b8d4994f5b95f2b5
Luc Bussiere <luc.bussiere@mac.com>
2024-10-03 14:34:20 +0200

    Created source file within src folder and project file

---
+++ src/BIO515LiveScript.R
@@ -0,0 +1,11 @@
+# This is a script for live coding and demonstrating
+# reproducible research in BIO515
+# created by Luc Bussiere
+# last modified 3 Oct 2024
+
+
+# clear the memory
+rm(list = ls())
+# garbage collection
+gc()
+
